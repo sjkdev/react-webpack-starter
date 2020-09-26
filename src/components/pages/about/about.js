0c00c327852459eaf86f7cd7c4f746bda149c51c
seanjamesking <helloseanking@yahoo.com>
2020-09-26 19:50:21 +0100

    added counter for demo purposes

--- src/components/pages/about/about.js
+++ src/components/pages/about/about.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Col, Row} from 'react-bootstrap'
 import { Helmet } from 'react-helmet'
 import './about.scss'
 
+const CountVal = [1];
+
 const about = () => {
+
+    const [count, setCount] = useState(0);
+
     return (
         <div>
        <Helmet>
@@ -10,6 +16,17 @@ const about = () => {
        <meta name="description" content="Helmet application, unique longtail keywords here and pther meta content and seo stuff" />
        </Helmet>
             <h1 className="about">About</h1>
+            <Row>
+                <Col>
+                    <div className="Counter">Count: {count}</div>
+                    <div>
+                        {CountVal.map(value => {
+                            return <button onClick={() => setCount(count + value)}>+{value}</button>
+                        })
+                        }
+                    </div>
+                </Col>
+            </Row>
         </div>
     )
 }
